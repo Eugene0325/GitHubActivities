fbfa21e4df0355088ac9f9495fe016c85e22ba40
Eugene <eugene050325@gmail.com>
2025-07-07 01:54:19 -0800

    Resolved merge conflict between median_branch and mean_branch

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
-print("Hello, world!")
+def median(numbers):
+    numbers.sort()
+    n = len(numbers)
+    mid = n // 2
+    if n % 2 == 0:
+        return (numbers[mid - 1] + numbers[mid]) / 2
+    else:
+        return numbers[mid]
+
+print("Median:", median([1, 2, 3, 4, 5]))
+
+def mean(numbers):
+    return sum(numbers) / len(numbers)
+
+print("Mean:", mean([1, 2, 3, 4, 5]))
