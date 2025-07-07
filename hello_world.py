c955d58fccfddb15d77c70187e468fc12a836d26
Eugene <eugene050325@gmail.com>
2025-07-07 01:48:07 -0800

    Added median function

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("Hello, world!")
+def median(numbers):
+    numbers.sort()
+    n = len(numbers)
+    mid = n // 2
+    if n % 2 == 0:
+        return (numbers[mid - 1] + numbers[mid]) / 2
+    else:
+        return numbers[mid]
+
+print("Median:", median([1, 2, 3, 4, 5]))
